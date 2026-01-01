ba81ff198711fbf0ac352ceb418fb8149795c090
rein-desu999 <122199781+rein-desu999@users.noreply.github.com>
2026-01-01 07:07:13 -0800

    hashrouter

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './app/App.jsx';
@@ -7,7 +8,9 @@ import ShopContextProvider from './Context/ShopContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <HashRouter>
     <ShopContextProvider>
-        <App/>
+      <App />
     </ShopContextProvider>
+  </HashRouter>
 );
